2fdce90b001639d0f321343c0de7ce64b02e6890
Fahil Ejaz <ejazfahil@gmail.com>
2022-10-05 15:15:00 +0200

    feat: add dbt trigger DAG

---
+++ dags/dbt_trigger_dag.py
@@ -0,0 +1,10 @@
+"""dbt trigger DAG 2022-10-05"""
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG("dbt_run", start_date=datetime(2022,11,1), schedule_interval="0 6 * * *",
+         default_args={"owner":"de"}, catchup=False) as dag:
+    run   = BashOperator(task_id="dbt_run",  bash_command="cd /dbt && dbt run  --target prod")
+    test  = BashOperator(task_id="dbt_test", bash_command="cd /dbt && dbt test --target prod")
+    run >> test
