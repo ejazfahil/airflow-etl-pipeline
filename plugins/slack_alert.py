301003efb9424781f4a05e7f772f2cbc998816e0
Fahil Ejaz <ejazfahil@gmail.com>
2022-10-12 16:30:00 +0200

    feat: add Slack alert on DAG failure

---
+++ plugins/slack_alert.py
@@ -0,0 +1,10 @@
+"""Slack alerts 2022-10-12"""
+import requests
+
+def slack_fail_alert(context):
+    dag_id = context["dag"].dag_id
+    task_id = context["task_instance"].task_id
+    exec_date = context["execution_date"]
+    msg = f":red_circle: *DAG Failed*\nDAG: `{dag_id}`\nTask: `{task_id}`\nDate: {exec_date:%Y-%m-%d}"
+    requests.post("https://hooks.slack.com/services/XXX/YYY/ZZZ",
+                  json={"text": msg}, timeout=10)
