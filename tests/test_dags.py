98ac52c28add510899b8768a038d046c78de863b
Fahil Ejaz <ejazfahil@gmail.com>
2022-10-20 09:15:00 +0200

    test: add DAG import and structure tests

---
+++ tests/test_dags.py
@@ -0,0 +1,16 @@
+"""DAG tests 2022-10-20"""
+from airflow.models import DagBag
+
+def test_no_import_errors():
+    bag = DagBag(dag_folder="dags/", include_examples=False)
+    assert len(bag.import_errors) == 0, str(bag.import_errors)
+
+def test_dag_count():
+    bag = DagBag(dag_folder="dags/", include_examples=False)
+    assert len(bag.dags) >= 3
+
+def test_csv_ingest_tasks():
+    bag = DagBag(dag_folder="dags/", include_examples=False)
+    dag = bag.get_dag("csv_ingest")
+    assert dag is not None
+    assert "ingest_sales" in dag.task_ids
